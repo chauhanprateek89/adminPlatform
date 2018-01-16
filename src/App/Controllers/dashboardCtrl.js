b8dec4edd1e807c4247d50cc2286402b0d877e2c
harsh phoujdar <d16123828@mydit.ie>
2018-01-16 20:19:08 +0000

    buggy dashboard

--- src/App/Controllers/dashboardCtrl.js
+++ src/App/Controllers/dashboardCtrl.js
@@ -8,19 +8,40 @@
 		function ($scope, toastr, spinner) {
 			$scope.activeTab = 'tab1';
 			$scope.activePill = 'pill1';
+			$scope.test = function(text) {
+				alert(text);
+			  }
 		
 			this.product = alerts_data;
 		
 		});
 
-		var alerts_data = {
-
-			name: 'ALert 1',
+		var alerts_data = [
+			{
+			name: 'Alert 1',
 			time: 10.35,
 			description: "Some issue needs your attention"
-
-		}
-
+		},
+			{
+			name: 'Alert 2',
+			time: 10.55,
+			description: "Some issue needs your attention"
+		},
+			{
+			name: 'Alert 3',
+			time: 10.55,
+			description: "Some issue needs your attention"
+		},
+			{
+			name: 'Alert 4',
+			time: 10.55,
+			description: "Some issue needs your attention"
+		},
+			{
+			name: 'Alert 5',
+			time: 10.55,
+			description: "Some issue needs your attention"
+		}]
 
 })();
 
