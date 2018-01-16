3b409244199db28af9d869a5ea1a9b9b12dc1feb
harsh phoujdar <d16123828@mydit.ie>
2018-01-16 12:46:09 +0000

    working_tabs

--- src/App/Controllers/dashboardCtrl.js
+++ src/App/Controllers/dashboardCtrl.js
@@ -1,44 +1,11 @@
+
 (function () {
 
 	'use strict';
 
 	angular.module('cognitionApp').controller('dashboardCtrl',
 		function ($scope, toastr, spinner) {
-
-			
-			
+			$scope.activeTab = 'tab1';
 		});
 })();
 
-// function dashboardCtrl($scope, $location) {
-// 	$scope.tabs = [
-// 		{ link : '#/jobs', label : 'Jobs' },
-// 		{ link : '#/invoices', label : 'Invoices' },
-// 		{ link : '#/payments', label : 'Payments' }
-// 	  ]; 
-	  
-// 	$scope.selectedTab = $scope.tabs[0];    
-// 	$scope.setSelectedTab = function(tab) {
-// 	  $scope.selectedTab = tab;
-// 	}
-	
-// 	$scope.tabClass = function(tab) {
-// 	  if ($scope.selectedTab == tab) {
-// 		return "active";
-// 	  } else {
-// 		return "";
-// 	  }
-// 	}
-//   }
-  
-//   function JobsCtrl($scope) {
-	
-//   }
-  
-//   function InvoicesCtrl($scope) {
-	
-//   }
-  
-//   function PaymentsCtrl($scope) {
-	
-//   }
